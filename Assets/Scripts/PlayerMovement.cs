25d585f4b593276a5646e7fe6278d30d410a2d67
Noah Ansel <ark217@gmail.com>
2015-11-25 20:03:44 -0600

    Uploading Unity work
    Currently, a basic player object and camera are implemented.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed;
+	public float east_west;
+	public float north_south;
+	
+	// Update is called once per frame
+	void Update () {
+		east_west = Input.GetAxis("Horizontal");
+		north_south = Input.GetAxis("Vertical");
+
+		transform.position += new Vector3 (east_west * speed, 0, north_south * speed);
+	}
+}
