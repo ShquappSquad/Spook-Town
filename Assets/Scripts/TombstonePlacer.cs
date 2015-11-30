8f5a1eaa87c5342dbe5b8d35f9836bf255f4a0d1
Noah Ansel <ark217@gmail.com>
2015-11-30 15:33:53 -0600

    Randomly generates trees

--- Assets/Scripts/TombstonePlacer.cs
+++ Assets/Scripts/TombstonePlacer.cs
@@ -12,10 +12,11 @@ public class TombstonePlacer : MonoBehaviour {
 	public GameObject grave1;
 	public GameObject grave2;
 	public GameObject grave3;
+	public GameObject tree;
 
 	// Use this for initialization
 	void Start () {
-		if (grave1 == null || grave2 == null || grave3 == null) {
+		if (grave1 == null || grave2 == null || grave3 == null || tree == null) {
 			return; // nothing to do here
 		}
 		// validation
@@ -45,21 +46,34 @@ public class TombstonePlacer : MonoBehaviour {
 				                          0,
 				                          ySpacing * y - ySpacing * yNumGraves/2 - leeway/2 + Random.value*leeway);
 				GameObject grave = null;
-				switch (Random.Range (0, 3)) {
-				case 0: {
-					grave = (GameObject)(Instantiate (grave1, transform.position + loc, Quaternion.identity));
-					break;
-				}
-				case 1: {
-					grave = (GameObject)(Instantiate (grave2, transform.position + loc, Quaternion.identity));
-					break;
-				}
+				switch (Random.Range (0, 10)) {
+				case 0:
+				case 1:
 				case 2: {
+					grave = (GameObject)(Instantiate (grave1, transform.position + loc, Quaternion.identity));
+					RotateTombstone (grave, Random.Range (0,12));
+					break;
+				}
+				case 3:
+				case 4:
+				case 5: {
+					grave = (GameObject)(Instantiate (grave2, transform.position + loc, Quaternion.identity));
+					RotateTombstone (grave, Random.Range (0,12));
+					break;
+				}
+				case 6:
+				case 7:
+				case 8: {
 					grave = (GameObject)(Instantiate (grave3, transform.position + loc, Quaternion.identity));
+					RotateTombstone (grave, Random.Range (0,12));
+					break;
+				}
+				case 9: {
+					grave = (GameObject)(Instantiate (tree, transform.position + loc + new Vector3(0, 3, 0), Quaternion.identity));
+					RotateTree (grave, Random.Range (0, 12));
 					break;
 				}
 				}
-				RotateTombstone (grave, Random.Range (0,9));
 			}
 		}
 	}
@@ -71,35 +85,100 @@ public class TombstonePlacer : MonoBehaviour {
 			break;
 		}
 		case 1: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 20.0f, 0.0f);
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 30.0f, 0.0f);
 			break;
 		}
 		case 2: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 40.0f, 0.0f);
-			break;
-		}
-		case 3: {
 			tombstone.transform.eulerAngles = new Vector3(0.0f, 60.0f, 0.0f);
 			break;
 		}
+		case 3: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+			break;
+		}
 		case 4: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 80.0f, 0.0f);
-			break;
-		}
-		case 5: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 100.0f, 0.0f);
-			break;
-		}
-		case 6: {
 			tombstone.transform.eulerAngles = new Vector3(0.0f, 120.0f, 0.0f);
 			break;
 		}
+		case 5: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 150.0f, 0.0f);
+			break;
+		}
+		case 6: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
+			break;
+		}
 		case 7: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 140.0f, 0.0f);
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 210.0f, 0.0f);
 			break;
 		}
 		case 8: {
-			tombstone.transform.eulerAngles = new Vector3(0.0f, 160.0f, 0.0f);
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 240.0f, 0.0f);
+			break;
+		}
+		case 9: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 270.0f, 0.0f);
+			break;
+		}
+		case 10: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 300.0f, 0.0f);
+			break;
+		}
+		case 11: {
+			tombstone.transform.eulerAngles = new Vector3(0.0f, 330.0f, 0.0f);
+			break;
+		}
+		}
+	}
+
+	void RotateTree(GameObject tombstone, int direction) {
+		switch (direction) {
+		case 0: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 0.0f, 0.0f);
+			break;
+		}
+		case 1: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 30.0f, 0.0f);
+			break;
+		}
+		case 2: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 60.0f, 0.0f);
+			break;
+		}
+		case 3: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 90.0f, 0.0f);
+			break;
+		}
+		case 4: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 120.0f, 0.0f);
+			break;
+		}
+		case 5: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 150.0f, 0.0f);
+			break;
+		}
+		case 6: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 180.0f, 0.0f);
+			break;
+		}
+		case 7: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 210.0f, 0.0f);
+			break;
+		}
+		case 8: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 240.0f, 0.0f);
+			break;
+		}
+		case 9: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 270.0f, 0.0f);
+			break;
+		}
+		case 10: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 300.0f, 0.0f);
+			break;
+		}
+		case 11: {
+			tombstone.transform.eulerAngles = new Vector3(270.0f, 330.0f, 0.0f);
 			break;
 		}
 		}
